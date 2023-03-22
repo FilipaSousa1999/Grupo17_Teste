ac9c38a1433bedf0f38a15765909052e0f133f64
[amandiofilipemoura] <[amandiomourajr@gmail.com]>
2023-03-22 17:31:33 +0000

    ci(build): add .gitignore and implement GH Actions for the project

--- pa-chat-room-main/server/src/main/java/ServerThread.java
+++ pa-chat-room-main/server/src/main/java/ServerThread.java
@@ -1,19 +1,13 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
+
+import  java.io.BufferedWriter;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import java.io.PrintWriter;
 
 
 public class ServerThread extends Thread {
@@ -47,58 +41,34 @@ public class ServerThread extends Thread {
         try {
             String msg;
             OutputStream ou = this.con.getOutputStream();
-            Writer out = new OutputStreamWriter(ou);
-            BufferedWriter bfw = new BufferedWriter(out);
+            OutputStreamWriter ouw = new OutputStreamWriter(ou);
+            BufferedWriter bfw = new BufferedWriter(ouw);
             clientes.add(bfw);
             nome = msg = bfr.readLine();
-        while ( !"Sair" .equalsIgnoreCase(msg) && msg!=null ) {
-          msg = bfr.readLine();
-          sendToAll(bfw, msg);
-          System.out.println(msg);
+            while ( !"Sair" .equalsIgnoreCase(msg) && msg!=null ) {
+                msg = bfr.readLine();
+                sendToAll(bfw, msg);
+                System.out.println(msg);
 
-        }
-    } catch ( IOException e ) {
-        e.printStackTrace ( );
-    }
-    }
-/**
- *Method to send message to all clients
- * @param bwSaida of type BufferedWriter
- * @param msg of type String
- * @throws IOException
- */
-public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
-    BufferedWriter bwS;
-    for (BufferedWriter bw : clientes) {
-        bwS = (BufferedWriter) bw;
-        if (!(bwSaida == bwS)) {
-            bw.write(nome + " -> " + msg + "\r\n");
-            bw.flush();
-        }
-    }
-}
-
-    public static void main (String[] args) {
-        //ServerThread server = new ServerThread ( 8888 );
-        try {
-            //creates the objects to instantiate the server
-            JLabel lblMessage = new JLabel("Porta do servidor: ");
-            JTextField txtPorta = new JTextField("8888");
-            Object[] texts = {lblMessage, txtPorta};
-            JOptionPane.showMessageDialog(null, texts);
-            server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
-            clientes = new ArrayList<BufferedWriter>();
-            //JOptionPane.showMessage(null, "Servidor ativo na porta: ")+txtPorta.getText();
-
-            while (true) {
-                System.out.println("Aguardar conexão...");
-                Socket con = server.accept();
-                System.out.println("Cliente conetado...");
-                Thread t = new ServerThread(con);
-                t.start();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch ( IOException e ) {
+            e.printStackTrace ( );
+        }
+    }
+    /**
+     *Method to send message to all clients
+     * @param bwSaida of type BufferedWriter
+     * @param msg of type String
+     * @throws IOException
+     */
+    public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
+        BufferedWriter bwS;
+        for (BufferedWriter bw : clientes) {
+            bwS = (BufferedWriter) bw;
+            if (!(bwSaida == bwS)) {
+                bw.write(nome + " -> " + msg + "\r\n");
+                bw.flush();
+            }
         }
     }
 }
